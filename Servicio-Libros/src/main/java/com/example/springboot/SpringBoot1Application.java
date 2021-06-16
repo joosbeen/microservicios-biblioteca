8729005bb5f903cef297d29602fa168f1a16b519
joosbeen <joosbeen@gmail.com>
2021-06-15 23:44:25 -0500

    Config Servicio-Libros

--- Servicio-Libros/src/main/java/com/example/springboot/SpringBoot1Application.java
+++ Servicio-Libros/src/main/java/com/example/springboot/SpringBoot1Application.java
@@ -2,12 +2,14 @@ package com.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringBoot1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBoot1Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot1Application.class, args);
+    }
 
 }
