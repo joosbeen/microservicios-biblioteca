06a2e79c30dc812a50c98d687331cc057b30ddca
joosbeen <joosbeen@gmail.com>
2021-06-17 21:39:33 -0500

    D

--- Zuul-Server/src/main/java/com/servicio/zuul/server/ZuulServerApplication.java
+++ Zuul-Server/src/main/java/com/servicio/zuul/server/ZuulServerApplication.java
@@ -1,19 +1,36 @@
 package com.servicio.zuul.server;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @EnableAuthorizationServer
 @EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
-public class ZuulServerApplication {
+public class ZuulServerApplication implements CommandLineRunner {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ZuulServerApplication.class, args);
-    }
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		String password = "12345";
+
+		for (int i = 0; i < 4; i++) {
+			String bcy = bCryptPasswordEncoder.encode("12345");
+			System.out.println(bcy);
+		}
+
+	}
 }
