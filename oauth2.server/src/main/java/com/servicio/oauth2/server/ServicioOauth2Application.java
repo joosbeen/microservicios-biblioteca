0629440f50bc012a6feb90bbaddd89784a9e71f9
joosbeen <joosbeen@gmail.com>
2021-06-17 22:18:54 -0500

    console pass oauth-server

--- oauth2.server/src/main/java/com/servicio/oauth2/server/ServicioOauth2Application.java
+++ oauth2.server/src/main/java/com/servicio/oauth2/server/ServicioOauth2Application.java
@@ -1,17 +1,35 @@
 package com.servicio.oauth2.server;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication(scanBasePackages = {"brave"})
-public class ServicioOauth2Application {
+public class ServicioOauth2Application implements CommandLineRunner {
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServicioOauth2Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		String pass = "";
+		
+		for (int i = 0; i < 5; i++) {
+			pass = bCryptPasswordEncoder.encode("12345");
+			System.out.println(pass);
+		}
+		
 	}
 
 }
